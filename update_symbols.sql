b69d4d93932db55c9304dfd1059013a790a966dc
mahmoud <mhagh66@gmail.com>
2023-03-10 22:58:26 +0330

    added my R,C symbols

---
+++ update_symbols.sql
@@ -0,0 +1,59 @@
+UPDATE [Capacitors - Ceramic - 0201]
+   SET [Library Path] = 'symbols/Passives/C0201.SCHLIB',
+       [Library Ref] = 'C0201';
+
+UPDATE [Capacitors - Ceramic - 0402]
+   SET [Library Path] = 'symbols/Passives/C0402.SCHLIB',
+       [Library Ref] = 'C0402';
+
+UPDATE [Capacitors - Ceramic - 0603]
+   SET [Library Path] = 'symbols/Passives/C0603.SCHLIB',
+       [Library Ref] = 'C0603';
+       
+UPDATE [Capacitors - Ceramic - 0805]
+   SET [Library Path] = 'symbols/Passives/C0805.SCHLIB',
+       [Library Ref] = 'C0805';
+       
+UPDATE [Capacitors - Ceramic - 1206]
+   SET [Library Path] = 'symbols/Passives/C1206.SCHLIB',
+       [Library Ref] = 'C1206';
+
+UPDATE [Capacitors - Ceramic - 1210]
+   SET [Library Path] = 'symbols/Passives/C1210.SCHLIB',
+       [Library Ref] = 'C1210';
+       
+UPDATE [Capacitors - Ceramic - 1812]
+   SET [Library Path] = 'symbols/Passives/C1812.SCHLIB',
+       [Library Ref] = 'C1812';
+
+UPDATE [Capacitors - Ceramic - 2220]
+   SET [Library Path] = 'symbols/Passives/C2220.SCHLIB',
+       [Library Ref] = 'C2220';
+
+UPDATE [Resistors - Surface Mount - 0201]
+   SET [Library Path] = 'symbols/Passives/R0201.SCHLIB',
+       [Library Ref] = 'R0201';
+
+UPDATE [Resistors - Surface Mount - 0402]
+   SET [Library Path] = 'symbols/Passives/R0402.SCHLIB',
+       [Library Ref] = 'R0402';
+
+UPDATE [Resistors - Surface Mount - 0603]
+   SET [Library Path] = 'symbols/Passives/R0603.SCHLIB',
+       [Library Ref] = 'R0603';
+
+UPDATE [Resistors - Surface Mount - 0805]
+   SET [Library Path] = 'symbols/Passives/R0805.SCHLIB',
+       [Library Ref] = 'R0805';
+
+UPDATE [Resistors - Surface Mount - 1206]
+   SET [Library Path] = 'symbols/Passives/R1206.SCHLIB',
+       [Library Ref] = 'R1206';
+
+UPDATE [Resistors - Surface Mount - 1210]
+   SET [Library Path] = 'symbols/Passives/R1210.SCHLIB',
+       [Library Ref] = 'R1210';
+
+UPDATE [Resistors - Surface Mount - 2512]
+   SET [Library Path] = 'symbols/Passives/R2512.SCHLIB',
+       [Library Ref] = 'R2512';
